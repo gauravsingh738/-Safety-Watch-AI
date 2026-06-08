8131c20ca0a57e1cc8547a0d2e07580d9e9bca83
gauravsingh738 <149926708+gauravsingh738@users.noreply.github.com>
2026-06-08 12:42:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js - DO NOT UPLOAD TO GITHUB
+const firebaseConfig = {
+  apiKey: "AIzaSyBclgRtoVbNlH-PslRwmrA2nEv10XY2w2M",
+  authDomain: "hotspot-d2c4d.firebaseapp.com",
+  projectId: "hotspot-d2c4d",
+  storageBucket: "hotspot-d2c4d.firebasestorage.app",
+  messagingSenderId: "679545250194",
+  appId: "1:679545250194:web:ceb3181d1487668df1a14b"
+};
